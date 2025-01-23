644092b5fd8b85855cbea8eea4299403e2a9b2a6
ald-diogo <adaomanuek@gmail.com>
2025-01-23 12:28:42 +0100

    Delete Counter

--- BlazorThemes/obj/Debug/net9.0/BlazorThemes.AssemblyInfo.cs
+++ BlazorThemes/obj/Debug/net9.0/BlazorThemes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorThemes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16fbb9c3987f2fadc98e2eab940bc3d75615d7bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorThemes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorThemes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
