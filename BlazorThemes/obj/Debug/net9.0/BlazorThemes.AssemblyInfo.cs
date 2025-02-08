911d5716936c3a98ecb5885abdf2b4c5bb1a3be0
ald-diogo <adaomanuek@gmail.com>
2025-02-08 21:16:34 +0100

    Add Recuperar Senha

--- BlazorThemes/obj/Debug/net9.0/BlazorThemes.AssemblyInfo.cs
+++ BlazorThemes/obj/Debug/net9.0/BlazorThemes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorThemes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a36a5a7c24f8905878477b91f6f65f79bd3ff59a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a27191e3ff94dcbb7ec9150a4fd3c9d6bfc970b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorThemes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorThemes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
