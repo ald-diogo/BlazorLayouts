11542dd7a91e2ad5c9ee1cc63129aec168444d4f
ald-diogo <adaomanuek@gmail.com>
2025-02-18 08:26:32 +0100

    Corrigi os conflitos

--- BlazorThemes/obj/Debug/net9.0/BlazorThemes.AssemblyInfo.cs
+++ BlazorThemes/obj/Debug/net9.0/BlazorThemes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorThemes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5294fb8078db0f4fda254c62681a91aece5d61fa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9cd26c22edb049d625d65db38edb6f2c0074bf5")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorThemes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorThemes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
