43c0bfbd710c38e370a1eae7e7cd96fa67843b29
ald-diogo <adaomanuek@gmail.com>
2025-02-19 08:47:40 +0100

    Template
    Add Routes.razor

--- BlazorThemes/Program.Main.cs
+++ BlazorThemes/Program.Main.cs
@@ -1,6 +1,22 @@
-﻿namespace BlazorThemes;
+﻿#if (IndividualLocalAuth)
+#if (UseServer)
+using Microsoft.AspNetCore.Components.Authorization;
+#endif
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+#endif
+using MudBlazor.Services;
+#if (UseWebAssembly && SampleContent)
+using BlazorThemes.Client.Pages;
+#endif
+using BlazorThemes.Components;
+#if (IndividualLocalAuth)
+using BlazorThemes.Components.Account;
+using BlazorThemes.Data;
+#endif
 
-public class Program_Main
+namespace BlazorThemes
 {
     
 }
+
