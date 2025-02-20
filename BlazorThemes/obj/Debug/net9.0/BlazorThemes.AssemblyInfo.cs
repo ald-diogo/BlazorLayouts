6e159b2c660182d89de11b37f2dd52cb4eef1649
ald-diogo <adaomanuek@gmail.com>
2025-02-20 10:33:27 +0100

    Update o arquivo Data

--- BlazorThemes/obj/Debug/net9.0/BlazorThemes.AssemblyInfo.cs
+++ BlazorThemes/obj/Debug/net9.0/BlazorThemes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorThemes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d7552736873786669f1f1c642328979a393906e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63563145d1b655765ab7d996f87b6fcf59329dfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorThemes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorThemes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
