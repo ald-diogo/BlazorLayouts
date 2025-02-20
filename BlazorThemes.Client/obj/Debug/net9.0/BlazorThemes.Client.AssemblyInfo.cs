7e2becd356037dbc7c59b81475c17c50094e6787
ald-diogo <adaomanuek@gmail.com>
2025-02-20 14:16:24 +0100

    Add Login

--- BlazorThemes.Client/obj/Debug/net9.0/BlazorThemes.Client.AssemblyInfo.cs
+++ BlazorThemes.Client/obj/Debug/net9.0/BlazorThemes.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorThemes.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7afc55d77a00cdd5265a7cb41fb1071d6821b3a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acd8bbe06d67382490b2e8d06cae9e60646ed753")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorThemes.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorThemes.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
