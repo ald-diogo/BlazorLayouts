736d948c2ecc200d344186474d031b3fe83e123a
ald-diogo <adaomanuek@gmail.com>
2025-02-21 15:57:22 +0100

    Template
    Add Routes.razor

--- BlazorThemes.Client/obj/Debug/net9.0/BlazorThemes.Client.AssemblyInfo.cs
+++ BlazorThemes.Client/obj/Debug/net9.0/BlazorThemes.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorThemes.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2733ec349423a7829e4b6c2000afc4206b51319d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e6a38a4a08027e8080dd3f214e32c4691a5b0e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorThemes.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorThemes.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
