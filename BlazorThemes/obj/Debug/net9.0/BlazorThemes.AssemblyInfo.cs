a636e97f1a35ef22b2668a1249e6da35f8eb5118
ald-diogo <adaomanuek@gmail.com>
2025-02-21 10:54:00 +0100

    Add ManageNavMenu.razor

--- BlazorThemes/obj/Debug/net9.0/BlazorThemes.AssemblyInfo.cs
+++ BlazorThemes/obj/Debug/net9.0/BlazorThemes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorThemes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2733ec349423a7829e4b6c2000afc4206b51319d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2137503b27eb8a83888766f52b8b9610902bb2f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorThemes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorThemes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
